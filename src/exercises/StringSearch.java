7701b69a1679d98d52d5777bbd1b0e51e5e33ed2
AtlasGeek7 <atlaswebtechie@gmail.com>
2019-11-06 22:34:12 -0600

    Modified my StringSearch class.

--- src/exercises/StringSearch.java
+++ src/exercises/StringSearch.java
@@ -15,15 +15,14 @@ public class StringSearch {
 		String term = input.nextLine();
 		input.close();
 		term = term.toLowerCase();
+		int idx = sentence.toLowerCase().indexOf(term);
+		int sentenceLen = sentence.length();
+		int termLen = term.length();
 		if (sentence.toLowerCase().contains(term)) {
 			System.out.println("The search term was found.");
-			int idx = sentence.toLowerCase().indexOf(term);
-			int sentenceLen = sentence.length();
-			int termLen = term.length();
 			System.out.println("Its index within the string is: " + idx + '.');
 			System.out.println("Its length is: " + termLen + " characters.");
 			System.out.println("Removing it from the sentence...");
-			String copy = "  " + sentence;
 			System.out.println(sentence.substring(0,idx) + sentence.substring(idx+termLen,sentenceLen));
 		} else {
 			System.out.println("The search term was not found.");
